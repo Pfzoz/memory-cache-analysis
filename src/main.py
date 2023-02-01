08db1bf546596c066f09a05e024536d81b6549d1
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2023-02-01 14:46:16 -0300

    refactor and colormap start

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import sys
+from colormap.colormap import colormap
+
+if(__name__ == "__main__"):
+    type = sys.argv[1]
+    if(type == "-c"):
+        colormap()
+        
+
+    elif(type == "-m"):
+        # c_file_name = 
+        # matrix_size_1 = 
+        # matrix_size_2 =
+        pass
