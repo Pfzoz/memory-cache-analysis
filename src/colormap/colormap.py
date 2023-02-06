fa0b889afa43b21c62602f3f229e9acb1ac80259
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2023-02-06 13:38:22 -0300

    finishing colormap

--- src/colormap/colormap.py
+++ src/colormap/colormap.py
@@ -7,14 +7,22 @@ import pandas as pd
 import csv
 
 def colormap():
-    path_file_1 = "./csv/o1.csv"
-    path_file_2 = "./csv/o2.csv"
-    path_file_3 = "./csv/o3.csv"
+    path_file_x = "./csv/ox.csv"
+    path_file_y = "./csv/oy.csv"
 
-    m1 = pd.read_csv(path_file_1, sep=";").to_numpy(dtype=np.float64)
-    m2 = pd.read_csv(path_file_2, sep=";").to_numpy(dtype=np.float64)
+    path_file_1 = "./csv/o2.csv"
+    path_file_2 = "./csv/o3.csv"
+    path_file_3 = "./csv/oy.csv"
 
-    mr = m1 - m1
+    
+    #m0 = pd.read_csv(path_file_0, sep=";").to_numpy(dtype=np.float64)
+    #m1 = pd.read_csv(path_file_1, sep=";").to_numpy(dtype=np.float64)
+    #m2 = pd.read_csv(path_file_2, sep=";").to_numpy(dtype=np.float64)
+    #m3 = pd.read_csv(path_file_3, sep=";").to_numpy(dtype=np.float64)
+    mx = pd.read_csv(path_file_x, sep=";").to_numpy(dtype=np.float64)
+    my = pd.read_csv(path_file_y, sep=";").to_numpy(dtype=np.float64)
+
+    mr = mx - my
 
     plt.rcParams["figure.figsize"] = [7.00, 3.50]
     plt.rcParams["figure.autolayout"] = True
