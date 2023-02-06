28cb6419986291783367c200ce181deb739afd6b
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2023-02-06 11:58:08 -0300

    changes

--- src/colormap/colormap.py
+++ src/colormap/colormap.py
@@ -1,4 +1,10 @@
+import csv
 
+def colormap(file_1, file_2, file_3):
+    path_file_1 = file_1
+    path_file_2 = "./csv/"+file_2
+    path_file_3 = "./csv/"+file_3
 
-def colormap(file_1, file_2):
-    f1 = open(file_1, "rw")
+    with open("o1.csv") as File:
+        Line_reader = csv.reader(File)
+        print(Line_reader)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import sys
 from colormap.colormap import colormap
 
+O1_FILE_NAME = "o1.csv"
+O2_FILE_NAME = "o2.csv"
+O3_FILE_NAME = "o3.csv"
+
 if(__name__ == "__main__"):
     type = sys.argv[1]
     if(type == "-c"):
-        colormap()
+        colormap(O1_FILE_NAME, O2_FILE_NAME, O3_FILE_NAME)
         
 
     elif(type == "-m"):
